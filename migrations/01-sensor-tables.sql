268fce181c14888ff26d8ed996ef3ed7c1242798
Berry Langerak <berry.langerak@gmail.com>
2020-05-03 16:07:48 +0200

    Removing unneeded tables for sensor_type and sensor_type_option.

--- migrations/01-sensor-tables.sql
+++ migrations/01-sensor-tables.sql
@@ -1,26 +1,7 @@
-CREATE TABLE sensor_type (
-  sensor_type_id TEXT PRIMARY KEY,
-  name TEXT NOT NULL
-);
-
-CREATE TABLE sensor_type_option (
-  sensor_type_option_id TEXT PRIMARY KEY,
-  sensor_type_id TEXT NOT NULL,
-  name TEXT NOT NULL,
-  type TEXT NOT NULL,
-  description TEXT NOT NULL,
-
-  FOREIGN KEY (sensor_type_id) 
-    REFERENCES sensor_type (sensor_type_id) 
-);
-
 CREATE TABLE sensor (
   sensor_id TEXT PRIMARY KEY,
   sensor_type TEXT NOT NULL,
-  name TEXT NOT NULL,
-
-  FOREIGN KEY (sensor_type)
-    REFERENCES sensor_type (type_id)
+  name TEXT NOT NULL
 );
 
 CREATE TABLE sensor_parameter (
